caf601aa125737ca5c0b2d61a27d910e111d1c42
Jon Janzen <jjjonjanzen@gmail.com>
2017-01-04 20:57:32 -0800

    Convert README.md to README.rst to be compliant with conventions Added very basic setup.py to test submission to PyPi

---
+++ README.rst
@@ -0,0 +1,8 @@
+================
+MidPines Project
+================
+
+This project is intended to encompass a mock `GroupMe <http://groupme.com>`_ server and client library. Named, respectively, UpperPines and LowerPines.
+Currently only LowerPines is has any work done on it.
+
+This is very much a work-in-progress and is **not** intended to be used in production environments.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='midpines',
+  packages=['midpines'],
+  version='0.1',
+  description='GroupMe API library',
+  author='Jonathan Janzen',
+  author_email='jjjonjanzen@gmail.com',
+  url='https://github.com/bigfootjon/midpines',
+  download_url='https://github.com/bigfootjon/midpines/tarball/0.1',
+  keywords=['api', 'groupme'],
+  classifiers=[],
+)
