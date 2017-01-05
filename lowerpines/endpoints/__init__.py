2045928eb9de897719bfabcdd667f157e6adcfa6
Jon Janzen <jjjonjanzen@gmail.com>
2017-01-04 23:19:51 -0800

    Update AbstractObject to support defining a dict of fields and automatically populating an object from json or another object

--- lowerpines/endpoints/__init__.py
+++ lowerpines/endpoints/__init__.py
@@ -55,11 +55,18 @@ class Request:
 
 
 class AbstractObject:
+    field_map = {}  # This is a dict of keys to values using: AbstractObject.KEY = json[VALUE]
+
     def save(self):
         raise NotImplementedError
 
     def _refresh_from_other(self, other):
-        raise NotImplementedError
+        for key, _ in self.field_map.items():
+            setattr(self, key, getattr(other, key))
+        self.on_fields_loaded()
+
+    def on_fields_loaded(self):
+        pass
 
     def refresh(self):
         raise NotImplementedError
@@ -69,5 +76,13 @@ class AbstractObject:
         raise NotImplementedError
 
     @classmethod
-    def from_json(cls, gmi, json):
-        raise NotImplementedError
+    def from_json(cls, gmi, json, *args):
+        obj = cls(gmi, *args)
+
+        for key, value_raw in obj.field_map.items():
+            json_val = json
+            for val in value_raw.split('.'):
+                json_val = json_val.get(val, None)
+            setattr(obj, key, json_val)
+        obj.on_fields_loaded()
+        return obj
