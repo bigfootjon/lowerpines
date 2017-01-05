5aeefbd9ebe98894f437fb15d660f3fefc39964f
Jon Janzen <jjjonjanzen@gmail.com>
2017-01-05 10:38:39 -0800

    Fix AbstractManager so that objects are only cached when needed

--- lowerpines/__init__.py
+++ lowerpines/__init__.py
@@ -5,18 +5,19 @@ VERSION = '0.1.1'
 
 class AbstractManager:
     def __len__(self):
+        self.lazy_fill_content()
         return self._content.__len__()
 
     def __getitem__(self, key):
+        self.lazy_fill_content()
         return self._content.__getitem__(key)
 
     def __iter__(self):
+        self.lazy_fill_content()
         return self._content.__iter__()
 
     def __init__(self, gmi, content=None):
         self.gmi = gmi
-        if content is None:
-            content = self._all()
         self._content = content
 
     def _all(self):
@@ -32,7 +33,12 @@ class AbstractManager:
             raise MultipleFoundException()
 
     def filter(self, **kwargs):
+        self.lazy_fill_content()
         filtered = self._content
         for arg, value in kwargs.items():
             filtered = [item for item in filtered if getattr(item, arg) == value]
         return filtered
+
+    def lazy_fill_content(self):
+        if self._content is None:
+            self._content = self._all()
