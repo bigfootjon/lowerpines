dc1bc3f26aa9cd01089ff559d4d1b8939dc22c80
Jon Janzen <jjjonjanzen@gmail.com>
2017-01-17 15:01:51 -0600

    Deleted stubs, they're not useful enough to keep around and updated

--- stubs/user.pyi
+++
@@ -1,9 +0,0 @@
-from lowerpines.endpoints.user import User
-from lowerpines.gmi import GMI
-
-
-class UserManager:
-    gmi = ... #  type: GMI
-
-    def __init__(self, gmi: GMI) -> None: ...
-    def get(self) -> User: ...
