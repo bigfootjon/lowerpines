a301ee401cbabcdfca227861e9a0928d7af93699
Jon Janzen <jjjonjanzen@gmail.com>
2017-11-16 23:36:05 -0600

    Update publish script

--- publish.py
+++ publish.py
@@ -5,7 +5,7 @@ import lowerpines
 if os.popen('git tag -l ' + lowerpines.VERSION).read() == '':
     os.system('git tag -a ' + lowerpines.VERSION + ' -m "Version '+lowerpines.VERSION + '"')
     os.system('git push --tags')
-    os.system('python setup.py sdist upload')
+    os.system('twine upload dist/*')
 else:
     print('There is already a tag for version ' + lowerpines.VERSION)
     exit(1)
