d4e73f38ceae7e113f1dcf05759ac761cf629d48
Jonathan Janzen <jjjonjanzen@gmail.com>
2019-10-08 23:13:59 -0500

    Add tests on block endpoint

---
+++ test/endpoints/test_block.py
@@ -0,0 +1,31 @@
+# pyre-strict
+from unittest import TestCase, mock
+
+from lowerpines.endpoints.block import Block
+from lowerpines.gmi import GMI
+
+
+class TestBlock(TestCase):
+    @mock.patch("lowerpines.endpoints.block.BlockIndexRequest.__init__")
+    def test_get_all(self, request_init) -> None:
+        request_init.side_effect = ValueError("marker")
+        with self.assertRaisesRegex(ValueError, "marker"):
+            Block(GMI("test")).get_all("dummy")
+
+    @mock.patch("lowerpines.endpoints.block.BlockBetweenRequest.__init__")
+    def test_block_exists(self, request_init) -> None:
+        request_init.side_effect = ValueError("marker")
+        with self.assertRaisesRegex(ValueError, "marker"):
+            Block.block_exists(GMI("test"), "dummy", "other")
+
+    @mock.patch("lowerpines.endpoints.block.BlockCreateRequest.__init__")
+    def test_block(self, request_init) -> None:
+        request_init.side_effect = ValueError("marker")
+        with self.assertRaisesRegex(ValueError, "marker"):
+            Block.block(GMI("test"), "dummy", "other")
+
+    @mock.patch("lowerpines.endpoints.block.BlockUnblockRequest.__init__")
+    def test_unblock(self, request_init) -> None:
+        request_init.side_effect = ValueError("marker")
+        with self.assertRaisesRegex(ValueError, "marker"):
+            Block.unblock(GMI("test"), "dummy", "other")
